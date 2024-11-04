8228e89cab2412c3835c037fd8f2cbeeb8591fd0
Vedang12345 <vedangchannel1@gmail.com>
2024-11-03 18:46:57 -0800

    Deleted unnecessary code from last year

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GalaxyBotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GalaxyBotTeleOp.java
@@ -19,7 +19,6 @@ import org.firstinspires.ftc.vision.VisionPortal;
 public class GalaxyBotTeleOp extends OpMode {
     private GalaxyBot robot;
 
-
     private Gamepad currentGamepad1 = new Gamepad();
     private Gamepad previousGamepad1 = new Gamepad();
 
@@ -82,77 +81,7 @@ public class GalaxyBotTeleOp extends OpMode {
         currentGamepad2.copy(gamepad2);
     }
 
-    private void spinSpinners() {
-        if(gamepad2.dpad_right ) {
-            robot.setSpinners(false, 0.01f);
-            direction = "moving up";
-        }
-        else if(gamepad2.dpad_left ) {
-            robot.setSpinners(true, 0.01f);
-            direction = "moving down";
-        }
-        else {
-            robot.setSpinners(true, 0.0f);
-            direction = "not moving";
-        }
-    }
-//
-//    private void liftRobot() {
-//        if (gamepad2.a && previousGamepad2.a) {
-//             robot.lift(1.0f, -1);
-//        }
-//        else if(gamepad2.b && previousGamepad2.b) {
-//             robot.lift(1.0f, 1);
-//        }
-//        else {
-//            robot.lift(0.0f, 0);
-//        }
-//    }
 
-    private void moveBox() {
-        if (gamepad2.y && !previousGamepad2.y) {
-            robot.closeBox();
-        }
-        if(gamepad2.x && !previousGamepad2.x) {
-            robot.openBox();
-        }
-    }
-    String direction = " ";
-    private void liftRobotSlide() {
-        if (gamepad2.dpad_up && previousGamepad2.dpad_up) {
-            robot.linearSlide(.8f, 1);
-
-        }
-        else if(gamepad2.dpad_down && previousGamepad2.dpad_down) {
-            robot.linearSlide(.8f, -1);
-        }
-        else {
-            robot.stopSlide();
-        }
-    }
-/*
-    private void doHanger() {
-        if(gamepad2.a) {
-            robot.moveHanger(.9f, 1);
-        }
-        if(gamepad2.b) {
-            robot.moveHanger(.9f, -1);
-        }
-        robot.moveHanger(0.0f, 0);
-    }*/
-//
-    private void intakePixel() {
-        if (gamepad2.right_bumper && !previousGamepad2.right_bumper) {
-           robot.intake();
-       }
-    }
-    boolean launched = false;
-//    private void shootPlane() {
-//        if(gamepad2.left_bumper && !launched) {
-//            launched = true;
-//            robot.launchAirplane();
-//        }
-//    }
     @Override
     public void init() {
         robot = new GalaxyBot(hardwareMap);
@@ -167,18 +96,12 @@ public class GalaxyBotTeleOp extends OpMode {
                         RevHubOrientationOnRobot.UsbFacingDirection.UP
                 )
         );
-        robot.closeBox();
         imu.initialize(myIMUparameters);
-
     }
-
-
     public void start() {
     }
     @Override
     public void loop() {
-
-
         try {
             copyGamepad();
         } catch (RobotCoreException e) {
@@ -186,22 +109,5 @@ public class GalaxyBotTeleOp extends OpMode {
         }
         double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
         mecanumDrive(botHeading);
-       // liftRobot();
-
-        intakePixel();
-        robot.doIntake();
-        liftRobotSlide();
-        moveBox();
-        //doHanger();
-        spinSpinners();
-        //shootPlane();
-        telemetry.addData("box open: ", robot.boxed);
-        telemetry.addData("spinner direction: ", direction);
-        telemetry.addData("launched: ", launched);
-        telemetry.addData("box pos: ", robot.box.getPosition());
-
-        telemetry.addData("leftSpinner pos: ", robot.leftSpinner.getPosition());
-        telemetry.addData("rightSpinner pos: ", robot.rightSpinner.getPosition());
-        telemetry.update();
     }
 }
